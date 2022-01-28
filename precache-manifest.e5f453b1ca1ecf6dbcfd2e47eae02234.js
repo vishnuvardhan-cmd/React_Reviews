687ba3147323b2130f67d04e60d40f4e806b8774
Vishnuvardhan Bandlamudi <bvishnu123vardhan@gmail.com>
2022-01-28 15:15:21 +0530

    Updates

---
+++ precache-manifest.e5f453b1ca1ecf6dbcfd2e47eae02234.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "699b1311fd29dce8baa20a5215367947",
+    "url": "/React_Reviews/index.html"
+  },
+  {
+    "revision": "85e64708ae9f98b9caad",
+    "url": "/React_Reviews/static/css/main.3d9f8e8d.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/React_Reviews/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_Reviews/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85e64708ae9f98b9caad",
+    "url": "/React_Reviews/static/js/main.39bafa25.chunk.js"
+  },
+  {
+    "revision": "1c1fa49ea31b769e2e39",
+    "url": "/React_Reviews/static/js/runtime-main.4d6231cb.js"
+  }
+]);
